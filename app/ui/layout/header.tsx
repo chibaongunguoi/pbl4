20da668c58bf4b7e6c234363c3cd3ad3e11e2979
chibaongunguoi <chibaongunguoi@gmail.com>
2025-09-16 10:27:38 +0700

    xóa c

--- app/ui/layout/header.tsx
+++ app/ui/layout/header.tsx
@@ -33,13 +33,13 @@ export default function Header() {
             <li>
               <div className="form-group form-icon-left">
                 <i className="icon-search form-icon"></i>{" "}
-                {/* <input
-                type="email"
-                name="text"
-                placeholder="Tìm kiếm việc làm nhận thưởng..."
-                value=""
-                className="form-control"
-              />{" "} */}
+                <input
+                  type="email"
+                  name="text"
+                  placeholder="Tìm kiếm việc làm nhận thưởng..."
+                  value=""
+                  className="form-control"
+                />{" "}
                 <button aria-label="Tìm kiếm" className="btn">
                   <i className="icon-arrow-right"></i>
                 </button>
@@ -133,7 +133,7 @@ export default function Header() {
               </ul>
             </li>{" "}
             <li>
-              <a href="/dang-nhap?callback=%2F" className="">
+              <a href="/login?callback=%2F" className="">
                 Đăng nhập
               </a>
             </li>{" "}
