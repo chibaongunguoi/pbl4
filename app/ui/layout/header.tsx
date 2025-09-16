cfb28192d8138c5efb11adbfe1b12d300bffeb9c
Dusk Sarchiy <dusqdn.eml@gmail.com>
2025-09-16 10:19:53 +0700

    add login auth and form

--- app/ui/layout/header.tsx
+++ app/ui/layout/header.tsx
@@ -31,13 +31,13 @@ export default function Header() {
           <li>
             <div className="form-group form-icon-left">
               <i className="icon-search form-icon"></i>{" "}
-              <input
+              {/* <input
                 type="email"
                 name="text"
                 placeholder="Tìm kiếm việc làm nhận thưởng..."
                 value=""
                 className="form-control"
-              />{" "}
+              />{" "} */}
               <button aria-label="Tìm kiếm" className="btn">
                 <i className="icon-arrow-right"></i>
               </button>
