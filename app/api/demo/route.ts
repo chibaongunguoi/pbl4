33323fbbd10f6ee4a94713d68cce20eec1620bd3
Dusk Sarchiy <dusqdn.eml@gmail.com>
2025-09-19 20:20:29 +0700

    fix find

--- app/api/demo/route.ts
+++ app/api/demo/route.ts
@@ -4,8 +4,6 @@ import connectDb from "@/app/lib/db";
 
 export async function POST(_: Request) {
     await connectDb();
-    const job = await JobDetail.findOne();
-    console.log(job)
-
+    const job = await JobDetail.find();
     return NextResponse.json({ success: true, data: job }, { status: 200 });
 }
