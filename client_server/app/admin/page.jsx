4b300ccf9f18757e11f72353d1ec04701799ac70
chibaongunguoi <chibaongunguoi@gmail.com>
2025-10-25 09:55:34 +0700

    chỉnh giao diện trang dashboard

--- client_server/app/admin/page.jsx
+++ client_server/app/admin/page.jsx
@@ -81,39 +81,18 @@ export default function AdminDashboard() {
 
       <div className="dashboard-stats">
         <div className="stat-card users">
-          <div className="stat-header">
-            <div className="stat-icon users">
-              <svg fill="white" viewBox="0 0 20 20" width="24" height="24">
-                <path d="M9 6a3 3 0 11-6 0 3 3 0 016 0zM17 6a3 3 0 11-6 0 3 3 0 016 0zM12.93 17c.046-.327.07-.66.07-1a6.97 6.97 0 00-1.5-4.33A5 5 0 0119 16v1h-6.07zM6 11a5 5 0 015 5v1H1v-1a5 5 0 015-5z"/>
-              </svg>
-            </div>
-          </div>
+          <div className="stat-label">Tổng số người dùng</div>
           <div className="stat-number">{stats.totalUsers}</div>
-          <div className="stat-label">Người dùng</div>
         </div>
 
         <div className="stat-card jobs">
-          <div className="stat-header">
-            <div className="stat-icon jobs">
-              <svg fill="white" viewBox="0 0 20 20" width="24" height="24">
-                <path fillRule="evenodd" d="M6 6V5a3 3 0 013-3h2a3 3 0 013 3v1h2a2 2 0 012 2v3.57A22.952 22.952 0 0110 13a22.95 22.95 0 01-8-1.43V8a2 2 0 012-2h2zm2-1a1 1 0 011-1h2a1 1 0 011 1v1H8V5zm1 5a1 1 0 011-1h.01a1 1 0 110 2H10a1 1 0 01-1-1z" clipRule="evenodd"/>
-              </svg>
-            </div>
-          </div>
+          <div className="stat-label">Tổng số công việc</div>
           <div className="stat-number">{stats.totalJobs}</div>
-          <div className="stat-label">Công việc</div>
         </div>
 
         <div className="stat-card">
-          <div className="stat-header">
-            <div className="stat-icon companies">
-              <svg fill="white" viewBox="0 0 20 20" width="24" height="24">
-                <path fillRule="evenodd" d="M4 4a2 2 0 00-2 2v8a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2H4zm3 3a1 1 0 011-1h4a1 1 0 110 2H8a1 1 0 01-1-1zm0 3a1 1 0 011-1h4a1 1 0 110 2H8a1 1 0 01-1-1z" clipRule="evenodd"/>
-              </svg>
-            </div>
-          </div>
+          <div className="stat-label">Tổng số công ty</div>
           <div className="stat-number">{stats.totalCompanies}</div>
-          <div className="stat-label">Công ty</div>
         </div>
       </div>
     </div>
