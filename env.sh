e3bfff8d98f16b40a3bfe4294076f69ff6c2077e
Dusk Sarchiy <dusqdn.eml@gmail.com>
2025-11-15 09:20:09 +0700

    change env in test

--- env.sh
+++ env.sh
@@ -1,6 +1,7 @@
 export DB_CONNECTION_STRING="mongodb://192.168.137.1:27017/pbl4_db"
 
 # Client-Server
+export CLIENT_SERVER_HOST="192.168.137.1"
 export CLIENT_SERVER_PORT=3000
 
 # Scraper
@@ -12,4 +13,3 @@ export CHATBOT_SYSTEM_HOST="192.168.137.1"
 export CHATBOT_SYSTEM_PORT=37002
 export CHATBOT_SYSTEM_MODEL_NAME="qwen3/qwen3-4b"
 export LM_STUDIO_API="http://192.168.137.1:1234/v1/chat/completions"
-
