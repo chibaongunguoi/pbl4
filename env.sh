36ef310e920a4aaa39a935bb1e25f4f5f4506487
Dusk Sarchiy <dusqdn.eml@gmail.com>
2025-11-15 07:16:57 +0700

    add env

--- env.sh
+++ env.sh
@@ -1,14 +1,14 @@
-export DB_CONNECTION_STRING="mongodb://localhost:27017/pbl4_db"
+export DB_CONNECTION_STRING="mongodb://192.168.137.1:27017/pbl4_db"
 
 # Client-Server
 export CLIENT_SERVER_PORT=3000
 
 # Scraper
-export SCRAPER_HOST="localhost"
+export SCRAPER_HOST="192.168.137.1"
 export SCRAPER_PORT=37001
 
 # Chatbot System
-export CHATBOT_SYSTEM_HOST="localhost"
+export CHATBOT_SYSTEM_HOST="192.168.137.1"
 export CHATBOT_SYSTEM_PORT=37002
 export CHATBOT_SYSTEM_MODEL_NAME="qwen3/qwen3-4b"
-export LM_STUDIO_API="http://localhost:1234/v1/chat/completions"
+export LM_STUDIO_API="http://192.168.137.1:1234/v1/chat/completions"
