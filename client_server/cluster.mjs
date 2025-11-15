c7a64b9ebf4560bf850a81c1de4266a431d3b9f9
Dusk Sarchiy <dusqdn.eml@gmail.com>
2025-11-15 10:55:08 +0700

    fixed cluster.mjs

--- client_server/cluster.mjs
+++ client_server/cluster.mjs
@@ -4,24 +4,25 @@ import http from 'http';
 import next from 'next';
 
 const numCPUs = cpus().length;
-const port = process.env.PORT || 3000;
+const basePort = parseInt(process.env.CLIENT_SERVER_PORT || "3000", 10);
+const ports = Array.from({ length: numCPUs }, (_, _i) => basePort);
+
 const app = next({ dev: false });
 const handle = app.getRequestHandler();
 
 if (cluster.isPrimary) {
   console.log(`Primary ${process.pid} is running`);
-
-  // Fork workers for each CPU core
-  for (let i = 0; i < numCPUs; i++) {
-    cluster.fork();
-  }
-
-  cluster.on('exit', (worker, _, __) => {
-    console.log(`Worker ${worker.process.pid} died. Restarting...`);
-    cluster.fork();
+  console.log("Port list:", ports);
+  ports.forEach(port => {
+    cluster.fork({ WORKER_PORT: port });
   });
-
+  cluster.on('exit', (worker) => {
+    const port = worker.process.env.WORKER_PORT;
+    console.log(`Worker ${worker.process.pid} on port ${port} died. Restarting...`);
+    cluster.fork({ WORKER_PORT: port });
+  });
 } else {
+  const port = process.env.WORKER_PORT;
   app.prepare().then(() => {
     http.createServer((req, res) => handle(req, res)).listen(port, () => {
       console.log(`Worker ${process.pid} started on port ${port}`);
