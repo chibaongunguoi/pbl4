31caab8a9ea9b8e90e369d8d1a0a1004ec0124c5
Dusk Sarchiy <dusqdn.eml@gmail.com>
2025-11-21 08:36:13 +0700

    hot fix: add sudo apt update

--- scripts/mongodb.sh
+++ scripts/mongodb.sh
@@ -3,6 +3,7 @@ curl -fsSL https://www.mongodb.org/static/pgp/server-7.0.asc |
     --dearmor
 echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
 
+sudo apt update
 sudo apt install -y mongodb-org
 sudo systemctl enable mongod
 sudo systemctl start mongod
