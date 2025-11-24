991ccc4cddc50e4fff7b46eb54e7e9e8cdcf8840
Dusk Sarchiy <dusqdn.eml@gmail.com>
2025-11-24 17:44:50 +0700

    modify termux.sh

--- ubuntu/termux.sh
+++ ubuntu/termux.sh
@@ -1,7 +1,7 @@
-pkg install -y curl gnupg nodejs npm python termux-services
+pkg install -y curl gnupg nodejs python termux-services
 
 curl -fsSL https://www.mongodb.org/static/pgp/server-7.0.asc |
-  sudo gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg \
+  gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg \
     --dearmor
 echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
 
