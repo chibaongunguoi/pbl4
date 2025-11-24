39acae4a72e3226d1c2126379791d77ff8d20c7e
Dusk Sarchiy <dusqdn.eml@gmail.com>
2025-11-24 17:46:07 +0700

    remove sudo in termux.sh

--- ubuntu/termux.sh
+++ ubuntu/termux.sh
@@ -3,7 +3,7 @@ pkg install -y curl gnupg nodejs python termux-services
 curl -fsSL https://www.mongodb.org/static/pgp/server-7.0.asc |
   gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg \
     --dearmor
-echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
+echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-7.0.list
 
 pkg update
 pkg install -y mongodb-org mongodb-database-tools
@@ -26,5 +26,5 @@ fi
 pip install -r requirements.txt
 
 cd client_server
-sudo npm install
-sudo npm run build
+npm install
+npm run build
