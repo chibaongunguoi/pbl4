7f52702de022480599a577fc0fb08bfbe8fa2022
Dusk Sarchiy <dusqdn.eml@gmail.com>
2025-11-24 17:43:42 +0700

    add termux setup

---
+++ ubuntu/termux.sh
@@ -0,0 +1,30 @@
+pkg install -y curl gnupg nodejs npm python termux-services
+
+curl -fsSL https://www.mongodb.org/static/pgp/server-7.0.asc |
+  sudo gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg \
+    --dearmor
+echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
+
+pkg update
+pkg install -y mongodb-org mongodb-database-tools
+sv-enable mongod
+sv up mongod
+. scripts/env.sh
+
+mongorestore --db=pbl4_db --uri=$DB_CONNECTION_STRING --drop ./database/seed/pbl4_db
+
+if [ ! -d ".venv" ]; then
+  python3 -m venv .venv
+fi
+
+if [ -d ".venv/Scripts" ]; then
+  . .venv/Scripts/activate
+elif [ -d ".venv/bin" ]; then
+  . .venv/bin/activate
+fi
+
+pip install -r requirements.txt
+
+cd client_server
+sudo npm install
+sudo npm run build
