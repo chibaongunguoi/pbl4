094e64543235d633eb3f8ac69f6d135cd7dd9f0f
Dusk Sarchiy <dusqdn.eml@gmail.com>
2025-12-15 22:13:59 +0700

    add database connection to the install script

--- windows/install.sh
+++ windows/install.sh
@@ -8,6 +8,16 @@ else
   exit
 fi
 
+cat >$TMP/mongod.cfg <<EOF
+storage:
+    dbPath: $HOME_DRIVE_LETTER:\\host\\pbl4_db
+net:
+    bindIp: localhost
+    port: 27017
+EOF
+
+mongod -f $TMP/mongod.cfg >/dev/null 2>&1 &
+
 . scripts/env.sh
 mongorestore --db=pbl4_db --uri=$DB_CONNECTION_STRING --drop ./database/seed/pbl4_db
 
