2f62ac010a6745bee2e60f7abe9366191403718b
Dusk Sarchiy <dusqdn.eml@gmail.com>
2025-12-22 09:31:44 +0700

    change prompt message

--- scripts/db.sh
+++ scripts/db.sh
@@ -1,2 +1,11 @@
+cat >$TMP/mongod.cfg <<EOF
+storage:
+    dbPath: $HOME_DRIVE_LETTER:\\host\\pbl4_db
+net:
+    bindIp: localhost
+    port: 27017
+EOF
+
+mongod -f $TMP/mongod.cfg >/dev/null 2>&1 &
 rm -r ./database/seed
 mongodump --host="localhost" --port=27017 --db=pbl4_db --out="./database/seed"
