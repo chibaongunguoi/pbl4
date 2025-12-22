5f68a5d6c4b12de5b3850e65198d8e265c855f74
Dusk Sarchiy <dusqdn.eml@gmail.com>
2025-12-22 08:56:36 +0700

    fix file helper

--- client_server/app/lib/fileHelpers.js
+++ client_server/app/lib/fileHelpers.js
@@ -2,6 +2,9 @@ export function getRawFileHref(cv) {
   if (cv.startsWith('http')) {
     return cv;
   }
+  if (cv.startsWith('/files')) {
+    return cv;
+  }
   const path = '/files' + cv;
   return path;
 }
