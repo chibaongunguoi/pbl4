db90d4734cb121d03620d0b86df7ff34fb451cd6
Dusk Sarchiy <dusqdn.eml@gmail.com>
2025-12-27 22:59:43 +0700

    add mongod filter

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -1,14 +1,17 @@
 sudo apt install -y curl gnupg nodejs npm python3 python3-venv
+if [ -z "$(which mongod)" ]; then
 
-curl -fsSL https://www.mongodb.org/static/pgp/server-7.0.asc |
-  sudo gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg \
-    --dearmor
-echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
+  curl -fsSL https://www.mongodb.org/static/pgp/server-7.0.asc |
+    sudo gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg \
+      --dearmor
+  echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
 
-sudo apt update
-sudo apt install -y mongodb-org mongodb-database-tools
-sudo systemctl enable mongod
-sudo systemctl start mongod
+  sudo apt update
+  sudo apt install -y mongodb-org mongodb-database-tools
+  sudo systemctl enable mongod
+  sudo systemctl start mongod
+fi
+
 . scripts/env.sh
 
 mongorestore --db=pbl4_db --uri=$DB_CONNECTION_STRING --drop ./database/seed/pbl4_db
